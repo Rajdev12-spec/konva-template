40efac469eb1987ab5c50076ae49bd7bd2eee535
Raj Sahani <rajsahani78200@gmail.com>
2026-01-21 15:20:00 +0530

    fix: fix the build error for update

--- src/editor/editor-view/index.tsx
+++ src/editor/editor-view/index.tsx
@@ -1,11 +1,10 @@
-import type { CanvasNode, CardNode, CarouselNode, ImageNode, LinkNode, ProfileCardNode, RectNode, TextNode, VideoNode } from "../../types/editor";
-import Canvas from "../canvas/Canvas"
+import type { CanvasNode } from "../../types/editor";
+import Canvas from "../canvas/Canvas";
 interface ViewModeProps {
     nodes: CanvasNode[];
     selectedId: string | null;
     previewMode: boolean;
-    updateNode: (id: string,
-        attrs: Partial<TextNode> | Partial<RectNode> | Partial<ImageNode> | Partial<VideoNode> | Partial<CarouselNode> | Partial<LinkNode> | Partial<CardNode> | Partial<ProfileCardNode>) => void;
+     updateNode: (id: string, attrs: Partial<CanvasNode>) => void
     setSelectedId: (id: string | null) => void;
     setNodes: React.Dispatch<React.SetStateAction<CanvasNode[]>>;
 }
